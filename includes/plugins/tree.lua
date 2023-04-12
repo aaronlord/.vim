b7ec95bc131776ad91a05cde842af96383bb4188
Aaron Lord <hire@aaron.codes>
2023-04-12 13:31:03 +0100

    Updated tree.lua

--- includes/plugins/tree.lua
+++ includes/plugins/tree.lua
@@ -1,3 +1,6 @@
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.6
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -8,7 +11,31 @@ require('nvim-tree').setup({
         cmd = 'open',
     },
     view = {
-        width = 30
+        float = {
+        enable = true,
+        open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * WIDTH_RATIO
+            local window_h = screen_h * HEIGHT_RATIO
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+            return {
+                border = 'solid',
+                style = 'minimal',
+                relative = 'editor',
+                row = center_y,
+                col = center_x,
+                width = window_w_int,
+                height = window_h_int,
+            }
+            end,
+        },
+        width = function()
+            return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+        end,
     },
     renderer = {
         group_empty = true,
