66023147910983776ec1bd63ee1af1f78d422c4c
Aaron Lord <hire@aaron.codes>
2023-08-28 22:58:21 +0100

    lsp keys

--- lua/aaronlord/plugins/lsp-zero.lua
+++ lua/aaronlord/plugins/lsp-zero.lua
@@ -11,6 +11,9 @@ return {
         { "hrsh7th/nvim-cmp" },
         { "hrsh7th/cmp-nvim-lsp" },
         { "L3MON4D3/LuaSnip" },
+
+        -- Lint
+        { "mfussenegger/nvim-lint" },
     },
     event = "BufReadPre",
     config = function ()
@@ -58,26 +61,33 @@ return {
         lsp.on_attach(function(client, bufnr)
             local opts = {buffer = bufnr, remap = false}
 
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            -- vim.keymap.set("n", "<leader>lws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
-            -- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            -- vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>lrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>lrn", function() vim.lsp.buf.rename() end, opts)
-            -- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-
-            vim.keymap.set({'n', 'x'}, '<leader>lf', function()
-                vim.lsp.buf.format({async = false, timeout_ms = 10000})
-            end, opts)
+            vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+            vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+            vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+            vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
+            vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
+            vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
+            vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, opts)
+            vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
+            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+            vim.keymap.set("n", "<F2>", function() vim.lsp.buf.rename() end, opts)
+            vim.keymap.set({'n', 'x'}, '<F3>', function() vim.lsp.buf.format({async = false, timeout_ms = 10000}) end, opts)
+            vim.keymap.set("n", "<F4>", function() vim.lsp.buf.code_action() end, opts)
         end)
+
+        require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
         lsp.setup()
 
         vim.diagnostic.config({
             virtual_text = true
+        })
+
+        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+            callback = function()
+                require("lint").try_lint()
+            end,
         })
     end
 } 
