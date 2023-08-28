40377423afc8aac85f0d5740a1a11313872f6d06
Aaron Lord <hire@aaron.codes>
2023-08-28 12:16:01 +0100

    Format

--- lua/aaronlord/plugins/lsp-zero.lua
+++ lua/aaronlord/plugins/lsp-zero.lua
@@ -68,6 +68,10 @@ return {
             vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
             vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
             vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+            vim.keymap.set({'n', 'x'}, '<leader>vf', function()
+                vim.lsp.buf.format({async = false, timeout_ms = 10000})
+            end, opts)
         end)
 
         lsp.setup()
