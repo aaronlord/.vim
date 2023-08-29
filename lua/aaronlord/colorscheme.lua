30656d726ed8561259cb6c1ff5c958053dc9b1b8
Aaron Lord <hire@aaron.codes>
2023-08-29 11:01:03 +0100

    onbuy filetypes

--- lua/aaronlord/colorscheme.lua
+++ lua/aaronlord/colorscheme.lua
@@ -12,3 +12,8 @@ vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
     pattern = {"*.blade.php"},
     command = "set ft=html",
 })
+
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+    pattern = {"*.controller", "*.model", "*.element", "*.layout", "*.view"},
+    command = "set ft=php",
+})
