88e9f64565ff68f153de7771fe32d749386e1288
Aaron Lord <hire@aaron.codes>
2023-08-31 00:15:10 +0100

    Removed deprecated fugitive cmds

--- lua/aaronlord/plugins/fugitive.lua
+++ lua/aaronlord/plugins/fugitive.lua
@@ -1,7 +1,7 @@
 return {
     "tpope/vim-fugitive",
     cmd = {
-        "G", "Git", "Gdiffsplit", "Gvdiffsplit", "Gedit", "Gsplit", "Gread",
-        "Gwrite", "Ggrep", "Glgrep", "Gmove", "Gdelete", "Gremove", "Gbrowse",
+        "G", "Git", "GRemove", "GDelete", "GMove", "GRename", "GBrowse",
+        "Gdiffsplit", "Ghdiffsplit", "Gvdiffsplit", "Gclog",
     },
 }
