bb2052bcab8d547825e3fb1efd899b88b6a8f55b
Aaron Lord <hire@aaron.codes>
2023-08-31 00:33:38 +0100

    GBrowse GitHub and GitLab

--- lua/aaronlord/plugins/fugitive.lua
+++ lua/aaronlord/plugins/fugitive.lua
@@ -1,5 +1,9 @@
 return {
     "tpope/vim-fugitive",
+    dependencies = {
+        "tpope/vim-rhubarb", -- GitHub
+        "shumphrey/fugitive-gitlab.vim", -- GitLab
+    },
     cmd = {
         "G", "Git", "GRemove", "GDelete", "GMove", "GRename", "GBrowse",
         "Gdiffsplit", "Ghdiffsplit", "Gvdiffsplit", "Gclog",
