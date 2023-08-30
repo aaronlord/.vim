559202de4ed833280edc3f9fce57767d32d1f6d7
Aaron Lord <hire@aaron.codes>
2023-08-31 00:10:22 +0100

    Installed fugitive

---
+++ lua/aaronlord/plugins/fugitive.lua
@@ -0,0 +1,7 @@
+return {
+    "tpope/vim-fugitive",
+    cmd = {
+        "G", "Git", "Gdiffsplit", "Gvdiffsplit", "Gedit", "Gsplit", "Gread",
+        "Gwrite", "Ggrep", "Glgrep", "Gmove", "Gdelete", "Gremove", "Gbrowse",
+    },
+}
