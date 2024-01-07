a45c3fd99b1e07a83a86494070448f4fcf73bf9a
Aaron Lord <hire@aaron.codes>
2024-01-07 22:18:14 +0000

    fidget

--- lua/aaronlord/plugins/lsp-zero.lua
+++ lua/aaronlord/plugins/lsp-zero.lua
@@ -15,12 +15,57 @@ return {
 
         -- Lint
         { "mfussenegger/nvim-lint" },
+
+        -- UI
+        { "j-hui/fidget.nvim" }
     },
     event = "BufReadPre",
     config = function ()
         local lsp = require("lsp-zero")
 
         lsp.preset("recommended")
+
+        lsp.set_preferences({
+            suggest_lsp_servers = false,
+        })
+
+        lsp.on_attach(function(client, bufnr)
+            local opts = {buffer = bufnr, remap = false}
+
+            vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+            vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+            vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+            vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
+            vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
+            vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
+            vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, opts)
+            vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
+            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+            vim.keymap.set("n", "<F2>", function() vim.lsp.buf.rename() end, opts)
+            vim.keymap.set({'n', 'x'}, '<F3>', function() vim.lsp.buf.format({async = false, timeout_ms = 10000}) end, opts)
+            vim.keymap.set("n", "<F4>", function() vim.lsp.buf.code_action() end, opts)
+        end)
+
+        local signs = {
+            Error = " ",
+            Warn = " ",
+            Hint = " ",
+            Info = ""
+        }
+
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
+
+        vim.diagnostic.config({
+            virtual_text = true,
+            underline = false,
+        })
+
+        lsp.setup()
 
         local cmp = require("cmp")
         local cmp_action = lsp.cmp_action()
@@ -48,48 +93,6 @@ return {
             },
         })
 
-        lsp.set_preferences({
-            suggest_lsp_servers = false,
-        })
-
-        local signs = {
-            Error = " ",
-            Warn = " ",
-            Hint = " ",
-            Info = ""
-        }
-
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-        end
-
-        lsp.on_attach(function(client, bufnr)
-            local opts = {buffer = bufnr, remap = false}
-
-            vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
-            vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
-            vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
-            vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, opts)
-            vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<F2>", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set({'n', 'x'}, '<F3>', function() vim.lsp.buf.format({async = false, timeout_ms = 10000}) end, opts)
-            vim.keymap.set("n", "<F4>", function() vim.lsp.buf.code_action() end, opts)
-        end)
-
-        lsp.setup()
-
-        vim.diagnostic.config({
-            virtual_text = true,
-            underline = false,
-        })
-
         vim.api.nvim_create_autocmd({ "BufWritePost" }, {
             callback = function()
                 require("lint").try_lint(nil, {
@@ -99,5 +102,7 @@ return {
         })
 
         require("luasnip.loaders.from_snipmate").lazy_load()
+
+        require("fidget").setup()
     end
-} 
+}
